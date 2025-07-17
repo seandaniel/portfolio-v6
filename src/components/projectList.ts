9d7047925e999d4ecd2489a159bc32404376aa75
seandaniel-uf <degroot.sd@gmail.com>
2025-07-17 19:49:15 -0400

    Fix: project description copy text

--- src/components/projectList.ts
+++ src/components/projectList.ts
@@ -2,19 +2,9 @@ import growthIcon from "../assets/growth.svg";
 import musicSceneIcon from "../assets/music-scene.svg";
 import golfIcon from "../assets/golf.svg";
 import notesIcon from "../assets/notes.svg";
-import sunIcon from "../assets/sun.svg";
-import fuelIcon from "../assets/rocket.svg";
-import mountainIcon from "../assets/mountain.svg";
+import keyboardIcon from "../assets/keyboard.svg"
 
 const projectList = [
-  {
-    icon: growthIcon,
-    link: "https://www.verbeekslandscaping.com",
-    title: "Verbeek's Farm & Garden Centre",
-    description: "Plants, boots, gardening tools, buy them here",
-    type: "Shop",
-    className: "verbeeks",
-  },
   {
     icon: musicSceneIcon,
     link: "https://receiptify-v2.netlify.app/",
@@ -22,6 +12,14 @@ const projectList = [
     description: "Download Spotify receipts, share with friends",
     type: "App",
     className: "receiptify",
+  },
+  {
+    icon: growthIcon,
+    link: "https://www.verbeekslandscaping.com",
+    title: "Verbeek's Farm & Garden Centre",
+    description: "Plants, boots, gardening tools, buy them here",
+    type: "Shop",
+    className: "verbeeks",
   },
   {
     icon: notesIcon,
@@ -32,36 +30,20 @@ const projectList = [
     className: "notes",
   },
   {
+    icon: keyboardIcon,
+    link: "https://seandaniel.github.io/git-check/",
+    title: "Git Check",
+    description: "Search coders, view their most used programming languages",
+    type: "App",
+    className: "gitcheck"
+  },
+  {
     icon: golfIcon,
     link: "https://goodthingsgolf.netlify.app/",
     title: "Good Things Golf",
-    description: "A masonry golf gallery",
+    description: "A masonry golf photo gallery",
     type: "Photos",
     className: "goodthingsgolf",
-  },
-  {
-    icon: sunIcon,
-    link: "https://open.spotify.com/playlist/5YgbIoNbuNGPTyThjHZPSY?si=663313e0302948f5",
-    title: "Zen",
-    description: "Lo-fi songs for flow state",
-    type: "Playlist",
-    className: "zen",
-  },
-  {
-    icon: fuelIcon,
-    link: "https://open.spotify.com/playlist/4uH2mfivSNIZeHXrndnzjx?si=d3d8f474ed874360",
-    title: "Fuel",
-    description: "Uptempo electronic beats to get hyped up",
-    type: "Playlist",
-    className: "fuel",
-  },
-  {
-    icon: mountainIcon,
-    link: "https://www.youtube.com/watch?v=HEabNO4aqWM",
-    title: "Decemburrr",
-    description: "A look into the white room (snowboarding)",
-    type: "Video",
-    className: "decemburrr",
   },
 ];
 
